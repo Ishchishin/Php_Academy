acad22c52b6f6163317efab94e2fac12196e99a9
Sergey Ishchishin <dolphin_i@ukr.net>
2016-12-03 21:11:33 +0200

    Ready

--- arrays_loops_tasks/18.php
+++ arrays_loops_tasks/18.php
@@ -4,4 +4,16 @@
  * User: Ishchishin
  * Date: 01.12.2016
  * Time: 22:33
+ * <p>18. Составьте массив дней недели. С помощью цикла foreach выведите все дни недели,
+ * выходные дни следует вывести жирным.</p>
  */
+
+$week=array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+foreach($week as $value ){
+    if ($value=='Saturday' || $value=='Sunday' ){
+        echo "<B>".$value."</B></br>";
+    }else{
+        echo $value."</br>";
+    }
+}
+?>
