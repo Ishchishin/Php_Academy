77fa30c9b216f99166d7b11efff97bc584f0e47e
Sergey Ishchishin <dolphin_i@ukr.net>
2016-12-12 00:26:30 +0200

    ready #7

--- functions_forms_tasks/7.php
+++ functions_forms_tasks/7.php
@@ -8,14 +8,21 @@
  * Создать гостевую книгу, где любой человек может оставить комментарий в текстовом поле и добавить его.
  *Все добавленные комментарии выводятся над текстовым полем.
  */
+if ((file_exists("test.txt"))&&(!empty("test.txt"))){
+    echo "<br>";
+    $put =file_get_contents("test.txt",true);
+    echo $put;
+    echo "<br>";
+}
 
 if (isset($_POST['msg'])){
     if (empty($_POST['coments']) ){
         echo "Achtung!!! Achtung!!! заполните коментарий";
     }else{
         $comment = $_POST['coments'];
-        file_put_contents("test.txt",$comment."<br>" , FILE_APPEND);
-    }
+    file_put_contents("test.txt",$comment."<br>" , FILE_APPEND);
+    echo $comment;
+}
 }
 ?>
 
@@ -27,12 +34,6 @@ if (isset($_POST['msg'])){
 </head>
 
 <body>
-<?php
-echo "<br>";
-$put =file_get_contents("test.txt",true);
-echo $put;
-echo "<br>";
-?>
 <form action="" method="post" >
     <textarea name="coments"></textarea>
     <br>
